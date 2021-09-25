051712c3a315fc9a5e332243485e9b7df04d2c35
samitha lakshan <50348300+samitha093@users.noreply.github.com>
2021-09-25 17:33:28 +0530

    final

---
+++ client/pages/login.tsx
@@ -0,0 +1,13 @@
+
+const login = () => {
+
+    return (
+        <div className='main'>
+            welcome
+            <hr />
+            <h1>sgjsh</h1>
+        </div>
+    );
+}
+
+export default login;
