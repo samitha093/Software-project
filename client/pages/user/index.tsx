69596000752d8a8d32090c1da246908475ca9b35
RayaRash <85346524+RayaRash@users.noreply.github.com>
2021-10-06 19:36:56 +0530

    start

---
+++ client/pages/user/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import Navbar from '../../components/Navbar'
+
+const index: NextPage = () => {
+    return (
+        <div>
+            <Navbar/>
+              <div className= "Sign">
+                  <div className = "user_container">
+                  
+
+
+
+
+                  </div>
+
+
+
+
+              </div>
+            
+        </div>
+            
+        
+    );
+}
+
+export default index;
