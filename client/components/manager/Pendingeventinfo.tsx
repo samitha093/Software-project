45717eb106d43df3fe3f432ba5a3c4bf7f5f7981
PoornaManujaya <85576788+PoornaManujaya@users.noreply.github.com>
2021-10-10 08:27:02 +0530

    Pending Events Window V2.1
    More info windows buttons were added
    Grid System  was added
    Styles sheet was edited

--- client/components/manager/Pendingeventinfo.tsx
+++ client/components/manager/Pendingeventinfo.tsx
@@ -4,32 +4,53 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PublishIcon from '@mui/icons-material/Publish';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
 
 export default function PendingEventsInfo() {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Typography> xd8 </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography> xd4 </Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography> xd4 </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography> xd8 </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </CardContent>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
+        <Stack spacing={2} direction="row" className="manager-c-pendingticketsinfo-buttons-stack"  >
+          <Button variant="contained" endIcon={<PublishIcon />}>
+            Publish
+          </Button>
+          <Button variant="outlined" startIcon={<DeleteIcon />}>
+            Decline
+          </Button>
+        </Stack>
       </CardActions>
     </Card>
   );
 }
+
+
