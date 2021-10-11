6d7a857b41c845ef96be065ff37b763592e00144
RayaRash <85346524+RayaRash@users.noreply.github.com>
2021-10-11 14:02:06 +0530

    user index page changed/ user/ login page deleted

--- client/pages/user/index.tsx
+++ client/pages/user/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import Navbar from '../../components/Navbar'
+import Tabcustom from '../../components/user/Tabcustom'
 import {Grid} from '@mui/material';
 
 const index: NextPage = () => {
     return (
         <div className="buyer-bg">
              <Navbar/>
-             
-             <div>
-                              
-            </div>
+             <Grid className= "index_container">
+              <div className = "main_container">
+                <Tabcustom/>
+              </div>   
+          </Grid>
               
         </div>
     );
