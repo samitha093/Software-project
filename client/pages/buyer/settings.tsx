c322a0b50fb95f39ab793efa3c9f1978ef6eb32e
samitha lakshan <50348300+samitha093@users.noreply.github.com>
2021-10-30 14:29:15 +0530

    create reset password

--- client/pages/buyer/settings.tsx
+++ client/pages/buyer/settings.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Sidebar from '../../components/buyer/Sidebar'
 import Navbar from '../../components/Navbar'
+import Resetpw from '../../components/buyer/Resetpw'
 const settings: NextPage = () => {
         return (
             <div className="buyer-bg">
                 <Navbar/>
                 <div className="buyer-index">
                     <Sidebar/>
-                    <div>
-                       <h1>Settings</h1> 
+                    <div className='buyer-setting'>
+                       <h1>Settings</h1>
+                       <div className='buyer-setting-container'><Resetpw /></div> 
+                       
                     </div>
                 </div>
             </div>
