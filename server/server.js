423b7bdc43eb211a8d4ccc030a4598787cf7ff62
samitha lakshan <50348300+samitha093@users.noreply.github.com>
2021-11-03 09:23:18 +0530

    server settings added

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router()
 const cors = require('cors');
@@ -12,11 +11,14 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
+
 
 app.listen(port, ()=>{
     console.log(`server is on port: ${port}`);
