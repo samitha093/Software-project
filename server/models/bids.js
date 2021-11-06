f69f369c1d61f562ae0a5b4d42516d51c1295ea1
samitha lakshan <50348300+samitha093@users.noreply.github.com>
2021-11-06 09:41:02 +0530

    update bid table schema

--- server/models/bids.js
+++ server/models/bids.js
@@ -4,10 +4,11 @@ mongoose.plugin(require('meanie-mongoose-to-json'));
 const schema = mongoose.Schema;
 
 const bidschema = new schema({
-    order_status: {type:Boolean, required:true},
     no_of_tickets: {type:Number, required:true},
-    paid_amount:{type:Number, required:true},
-    user_id:{type:String, required:true}
+    bid_amount: {type:Number, required:true},
+    ticket_level_id:{type:String, required:true},
+    user_id:{type:String, required:true},
+    order_id:{type:String, required:false}
 },{
     timestamps:true
 });
