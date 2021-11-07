64e2cd74b5bf94e15dd6b27b2ce601157810c4d8
PoornaManujaya <85576788+PoornaManujaya@users.noreply.github.com>
2021-11-07 11:35:22 +0530

    Seller Approve Routing Created
    There is a issue. "Updated" message is displayed. But data is not updated in MongoDB. Please check it.

--- server/routes/manager.js
+++ server/routes/manager.js
@@ -3,7 +3,7 @@ const events = require('../models/events');
 const ticketLevels = require('../models/ticketLevels');
 const users = require('../models/users');
 
-/*Routing to search pending events*/
+/*Routing to Search Pending Events*/
 router.route('/pending').get((req,res) => {
     events.find({status: "pending"})
         .then(data => res.json(data))
@@ -11,7 +11,7 @@ router.route('/pending').get((req,res) => {
 
 });
 
-/*Routing to search active events*/
+/*Routing to Search Active Events*/
 router.route('/active').get((req,res) => {
     events.find({status: "active"})
         .then(data => res.json(data))
@@ -19,17 +19,9 @@ router.route('/active').get((req,res) => {
 
 });
 
-/*Routing to search declined events*/
+/*Routing to Search Declined Events*/
 router.route('/declined').get((req,res) => {
     events.find({status: "declined"})
-        .then(data => res.json(data))
-        .catch(err => res.status(400).json(err))
-
-});
-
-/*Routing to search seller data to approve*/
-router.route('/sellerapprove').get((req,res) => {
-    users.find({status: "false"})
         .then(data => res.json(data))
         .catch(err => res.status(400).json(err))
 
@@ -47,6 +39,42 @@ router.route('/ticketdetails/:id').get((req,res) => {
 router.route('/organizer/:id').get((req,res) => {
     users.find({user_id:req.params.id})
         .then(data => res.json(data))
+        .catch(err => res.status(400).json(err))
+});
+
+
+/*Routing to Search Seller Data to Approve*/
+router.route('/sellerapprove').get((req,res) => {
+    users.find({status: "false"})
+        .then(data => res.json(data))
+        .catch(err => res.status(400).json(err))
+
+});
+
+/*Update Seller Status to "True"*/
+router.route('/sellerapproveupdate/:id').put((req,res) => {
+    const user_name = req.body.name;
+    const email = req.body.email;
+    const contact = req.body.contact;
+    const password = req.body.password;
+    const user_type = req.body.userType;
+    const status = req.body.userStatus;
+    const user_id = req.body.UserId
+
+    users.findById(req.params.id)
+        .then(data =>{
+            data.name = user_name;
+            data.email = email;
+            data.contact = contact;
+            data.password = password;
+            data.userType = user_type;
+            data.userStatus = status;
+            data.UserId = user_id;
+
+            data.save()
+                .then(()=> res.status(200).json("data updated"))
+                .catch(err => res.status(400).json(err))
+        })
         .catch(err => res.status(400).json(err))
 });
 
