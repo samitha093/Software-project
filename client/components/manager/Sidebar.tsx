2f8f0dae0a079a2e2f076864643da0596f51041a
PoornaManujaya <85576788+PoornaManujaya@users.noreply.github.com>
2021-12-26 19:02:33 +0530

    Sidebar Updated

--- client/components/manager/Sidebar.tsx
+++ client/components/manager/Sidebar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 import Icon from '@mdi/react';
-import { mdiLanPending } from '@mdi/js';
-import { mdiCheckboxMultipleOutline } from '@mdi/js';
-import { mdiCloseBoxMultipleOutline } from '@mdi/js';
+import { mdiHomeOutline } from '@mdi/js';
+import { mdiCalendarSearch } from '@mdi/js';
 import { mdiCogs } from '@mdi/js';
 import { mdiLogout } from '@mdi/js';
+import { mdiAccountGroup } from '@mdi/js';
 import Tooltip from '@mui/material/Tooltip';
 import { endsession } from '../../session/Session';
 import { useRouter } from 'next/router';
@@ -26,40 +26,41 @@ const Sidebar: React.FC<SidebarProps> = ({ }) => {
             <div className="manager-c-sidebar-container">
 
                 <Link href="/manager">
-                    <Tooltip title="Pending Events" placement="bottom-end">
+                    <Tooltip title="Home Page" placement="bottom-end">
                         <div className="manager-c-sidebar-item">
-                            <Icon path={mdiLanPending} color='white' />
+                            <Icon className="manager-c-sidebar-item-icon" path={mdiHomeOutline} />
                         </div>
                     </Tooltip>
                 </Link>
 
-                <Link href="/manager/activeevents">
-                    <Tooltip title="Active Events" placement="bottom-end">
+                <Link href="/manager/pendingevents">
+                    <Tooltip title="Events" placement="bottom-end">
                         <div className="manager-c-sidebar-item">
-                            <Icon path={mdiCheckboxMultipleOutline} color='white' />
+                            <Icon className="manager-c-sidebar-item-icon" path={mdiCalendarSearch} />
                         </div>
                     </Tooltip>
                 </Link>
 
-                <Link href="/manager/declinedevents">
-                    <Tooltip title="Declined Events" placement="bottom-end">
+                <Link href="/manager/sellers">
+                    <Tooltip title="Sellers" placement="bottom-end">
                         <div className="manager-c-sidebar-item">
-                            <Icon path={mdiCloseBoxMultipleOutline} color='white' />
+                            <Icon className="manager-c-sidebar-item-icon" path={mdiAccountGroup} />
                         </div>
                     </Tooltip>
                 </Link>
+
 
                 <Link href="/manager/settings">
                     <Tooltip title="Manager Settings" placement="bottom-end">
                         <div className="manager-c-sidebar-item">
-                            <Icon path={mdiCogs} color='white' />
+                            <Icon className="manager-c-sidebar-item-icon" path={mdiCogs} />
                         </div>
                     </Tooltip>
                 </Link>
 
                 <Tooltip title="Logout from Manager" placement="bottom-end">
                     <div onClick={logout} className="manager-c-sidebar-item">
-                        <Icon path={mdiLogout} color='white' />
+                        <Icon className="manager-c-sidebar-item-icon" path={mdiLogout} />
                     </div>
                 </Tooltip>
             </div>
