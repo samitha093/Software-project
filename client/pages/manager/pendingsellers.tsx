45a2a598e0f58a3905a6680c833e79e5e1e88001
PoornaManujaya <85576788+PoornaManujaya@users.noreply.github.com>
2021-12-29 11:42:30 +0530

    Updated Sellers Pages (Manager Side)

--- client/pages/manager/pendingsellers.tsx
+++ client/pages/manager/pendingsellers.tsx
@@ -22,7 +22,7 @@ const pendingsellers: NextPage = () => {
                 <SellersTopBar id3='2' />
                 <div>
                     <h1>Pending Sellers</h1>
-                    <div className="manager-index-container">
+                    <div className="manager-sellers-container">
                         <SellerActions />
                     </div>
                 </div>
