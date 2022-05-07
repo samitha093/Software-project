84bb5d9129911d673f557c43662983f197a33593
samitha lakshan <samithalpathiraja@gmail.com>
2022-05-07 11:30:30 +0530

    update

--- client/node_modules/@mdi/react/dist/Icon.d.ts
+++
@@ -1,5 +0,0 @@
-import { FunctionComponent } from "react";
-import { IconProps } from './IconProps';
-export { default as Stack } from './Stack';
-export declare const Icon: FunctionComponent<IconProps>;
-export default Icon;
--- client/node_modules/@mdi/react/dist/Stack.d.ts
+++
@@ -1,4 +0,0 @@
-import { FunctionComponent } from "react";
-import { StackProps } from './StackProps';
-declare const Stack: FunctionComponent<StackProps>;
-export default Stack;
