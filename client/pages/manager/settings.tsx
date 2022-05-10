dd139285752881148d85b802e6dda7e0a431fc16
P.M.Amarasingha <85576788+PoornaManujaya@users.noreply.github.com>
2022-05-10 13:07:12 +0530

    Manager's settings page updated

--- client/pages/manager/settings.tsx
+++ client/pages/manager/settings.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Sidebar from '../../components/manager/Sidebar'
 import Passwordresetmanager from '../../components/manager/Passwordresetmanager'
+import Categorylist from '../../components/manager/Categorylist'
 import Navbar from '../../components/Navbar'
 import axios from 'axios'
-import {gethost} from '../../session/Session';
+import { gethost } from '../../session/Session';
 
 import styles from './styles.module.css'
 import classnames from 'classnames';
@@ -18,17 +19,17 @@ const settings: NextPage = () => {
     }, [])
     return (
         <div className={styles.manager_settings_bg}>
-                <Navbar/>
-                <div className={styles.manager_index}>
-                    <Sidebar id = '4'/>
-                    <div>
-                       <h1>Settings</h1>
-                       <div className={styles.manager_index_container}>
-                            <Passwordresetmanager/>
-                        </div> 
+            <Navbar />
+            <div className={styles.manager_index}>
+                <Sidebar id='4' />
+                <div>
+                    <h1>Settings</h1>
+                    <div className={styles.manager_index_container}>
+                        <Passwordresetmanager />
                     </div>
                 </div>
             </div>
+        </div>
 
     );
 }
