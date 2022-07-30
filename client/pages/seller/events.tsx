78c94124b964b7d08833ecd77d6f88e224824a44
samitha lakshan <samithalpathiraja@gmail.com>
2022-07-30 22:32:49 +0530

    fix: 🐛 404 error code update

--- client/pages/seller/events.tsx
+++ client/pages/seller/events.tsx
@@ -16,6 +16,8 @@ const Pendingevents: NextPage = () => {
     const [open, setopen] = React.useState(false);
     const router = useRouter();
     const [items, setitem] = React.useState([])
+    const [itemURL, setitemURL] = React.useState('s/getevent/pending');
+    const [itemID, setitemID] = React.useState("1");
     React.useEffect(()=>{
       axios.get(gethost() + 'a/refreshtoken',{withCredentials:true})
         .then(async (res)=>{
@@ -30,7 +32,7 @@ const Pendingevents: NextPage = () => {
                     const config = {
                       headers: { Authorization: `Bearer ${res.data.accesstoken}` }
                     };
-                    axios.get(gethost()+'s/getevent/PENDING',config).then(async (res)=>{
+                    axios.get(gethost() + itemURL ,config).then(async (res)=>{
                       await setitem(res.data)
                     }).catch(()=>{
                       Swal.fire({
@@ -61,14 +63,28 @@ const Pendingevents: NextPage = () => {
     const listitem = items.map((item:any)=>(
       <Popup data={item} key={item.id}/>
     ));
-
+    const changeSellerList = async(e:any)=>{
+      if(e == 1){
+          setitemURL('s/getevent/pending');
+          setitemID("1")
+      }else if(e == 2){
+          setitemURL('s/getevent/active');
+          setitemID("2")
+      }else if(e == 3){
+          setitemURL('s/getevent/end');
+          setitemID("3")
+      }else if(e == 4){
+          setitemURL('s/getevent/declined');
+          setitemID("4")
+      }
+    };
     return (
       <div className={Styles.seller_bg}>
         {open?<div>
         <Navbar/>
         <div className={Styles.seller_index}>
           <Sidebar id="2"/>
-          <Topbar id2="1"/>
+          <Topbar id="1" data={{change: changeSellerList}}/>
           <div className={Styles.seller_index_parent_fix}>
           <div className={Styles.seller_index_parent}>
             <h1 className={Styles.seller_index_container_name}>Pending Events</h1>
