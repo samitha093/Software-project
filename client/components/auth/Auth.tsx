10c2ee75f803b3a5b9170dd5d9173483d5fb8890
samitha lakshan <samithalpathiraja@gmail.com>
2022-08-05 13:43:38 +0530

    fix: 🐛 refresh token when multiple thred

--- client/components/auth/Auth.tsx
+++ client/components/auth/Auth.tsx
@@ -1,12 +1,39 @@
+//node packages
 import React from 'react'
+import {useRouter} from 'next/router'
+import axios from 'axios';
+import Swal from 'sweetalert2'
+//Session and local storage data
+import {gethost} from '../../session/Session';
 
 interface AuthProps {
 type:string;
 }
 
 const Auth: React.FC<AuthProps> = ({type, children}) => {
+
+    const router = useRouter();
+
+    const [open, setopen] = React.useState(false);
+
+    React.useEffect(()=>{
+        axios.get(gethost() + 'a/refreshtoken',{withCredentials:true}).then(async (res)=>{
+            if(res.data.type == type){
+              setopen(true);
+            }
+        }).catch((err)=>{
+          Swal.fire({
+            icon: 'error',
+            title: 'Authentication Failed',
+            text: err,
+            showConfirmButton: false,
+            timer: 2500
+          })
+          router.push('/user');
+        })       
+    },[])
     return (
-        <div>{children}</div>
+        <div>{open?children:null}</div>
     );
 }
 
