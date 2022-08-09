eae18d864c1b5ed3cc385c38febe23c20b34b56b
P.M.Amarasingha <85576788+PoornaManujaya@users.noreply.github.com>
2022-08-09 15:07:22 +0530

    Bugs Fixed- Manager

--- client/pages/manager/index.tsx
+++ client/pages/manager/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import Swal from 'sweetalert2'
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
 import axios from 'axios'
 //custom components
 import Sidebar from '../../components/manager/Sidebar'
@@ -17,13 +17,14 @@ import Seller from '../../components/manager/tables/Seller'
 import SellersTopBar from '../../components/manager/SellersTopBar'
 import Auth from '../../components/auth/Auth'
 //Session and local storage data
-import {gethost} from '../../session/Session';
+import { gethost } from '../../session/Session';
 //stylesheet
 import styles from './styles.module.css'
 
 const index: NextPage = function ActiveEvents() {
 
-    const [buyersidebar,setbuyersidebar] = React.useState([{status:true},{status:false},{status:false},{status:false}]);
+    const [buyersidebar, setbuyersidebar] = React.useState([{ status: true }, { status: false }, { status: false }, { status: false }]);
+
     const [itemID, setitemID] = React.useState("1");
     const [itemURL, setitemURL] = React.useState('m/getevent/pending');
     const [items, setitem] = React.useState([])
@@ -34,30 +35,30 @@ const index: NextPage = function ActiveEvents() {
 
     const [loader, setloader] = React.useState(false);
 
-    React.useEffect(()=>{
-       console.log("loading call")
-             
-    },[loader])
+    React.useEffect(() => {
+        console.log("loading call")
 
-    React.useEffect(()=>{
-        axios.get(gethost() + 'a/refreshtoken',{withCredentials:true}).then(async (res)=>{
-        const config = {
-            headers: { Authorization: `Bearer ${res.data.accesstoken}` }
+    }, [loader])
+
+    React.useEffect(() => {
+        axios.get(gethost() + 'a/refreshtoken', { withCredentials: true }).then(async (res) => {
+            const config = {
+                headers: { Authorization: `Bearer ${res.data.accesstoken}` }
             };
-            axios.get(gethost() + itemURL ,config).then(async (res) => {
+            axios.get(gethost() + itemURL, config).then(async (res) => {
                 await setitem(res.data)
             })
-            .catch(() => {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Database connection error!'
+                .catch(() => {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Database connection error!'
+                    })
                 })
-            })
         })
-        .catch((err)=>{}) 
-             
-    },[itemURL])
+            .catch((err) => { })
+
+    }, [itemURL])
 
     React.useEffect(() => {
         setloader(true);
@@ -65,10 +66,10 @@ const index: NextPage = function ActiveEvents() {
             const config = {
                 headers: { Authorization: `Bearer ${res.data.accesstoken}` }
             };
-            await axios.get(gethost() + selleritemURL,config)
+            await axios.get(gethost() + selleritemURL, config)
                 .then(async (res) => {
                     console.log(res.data)
-                    setselleritem(res.data); 
+                    setselleritem(res.data);
                     setloader(false);
                 })
                 .catch(() => {
@@ -78,121 +79,122 @@ const index: NextPage = function ActiveEvents() {
                         text: 'Database connection error!'
                     })
                 })
-            })
-            .catch((err) => {})
+        })
+            .catch((err) => { })
     }, [selleritemURL])
 
-      //component changer
-        const selectcomponents = async(e:any)=>{
-            tabchange(e);
-        };
+    //component changer
+    const selectcomponents = async (e: any) => {
+        tabchange(e);
+    };
 
-        async function tabchange(id:any){
-            var datapack = [
-                {status:id=='1'?true:false},
-                {status:id=='2'?true:false},
-                {status:id=='3'?true:false},
-                {status:id=='4'?true:false},
-            ]
-            setbuyersidebar(datapack);  
+    async function tabchange(id: any) {
+        var datapack = [
+            { status: id == '1' ? true : false },
+            { status: id == '2' ? true : false },
+            { status: id == '3' ? true : false },
+            { status: id == '4' ? true : false },
+        ]
+        setbuyersidebar(datapack);
+    }
+
+    const changeSellerList = async (e: any) => {
+        if (e == 0) {
+            setselleritemURL('m/userlist/all');
+            setselleritemID("0")
+        } else if (e == 1) {
+            setselleritemURL('m/userlist/pending');
+            setselleritemID("1")
+        } else if (e == 2) {
+            setselleritemURL('m/userlist/active');
+            setselleritemID("2")
+        } else if (e == 3) {
+            setselleritemURL('m/userlist/declined');
+            setselleritemID("3")
+        } else if (e == 4) {
+            setselleritemURL('m/userlist/unverified');
+            setselleritemID("4")
         }
+    };
 
-        const changeSellerList = async(e:any)=>{
-            if(e == 0){
-                setselleritemURL('m/userlist/all');
-                setselleritemID("0")
-            }else if(e == 1){
-                setselleritemURL('m/userlist/pending');
-                setselleritemID("1")
-            }else if(e == 2){
-                setselleritemURL('m/userlist/active');
-                setselleritemID("2")
-            }else if(e == 3){
-                setselleritemURL('m/userlist/declined');
-                setselleritemID("3")
-            }else if(e == 4){
-                setselleritemURL('m/userlist/unverified');
-                setselleritemID("4")
-            }
-        };
+    const changeEventList = async (e: any) => {
+        if (e == 1) {
+            setitemURL('m/getevent/pending');
+            setitemID("1")
+        } else if (e == 2) {
+            setitemURL('m/getevent/active');
+            setitemID("2")
+        } else if (e == 3) {
+            setitemURL('m/getevent/declined');
+            setitemID("3")
+        }
+    };
 
-        const changeEventList = async(e:any)=>{
-            if(e == 1){
-                setitemURL('m/getevent/pending');
-                setitemID("1")
-            }else if(e == 2){
-                setitemURL('m/getevent/active');
-                setitemID("2")
-            }else if(e == 3){
-                setitemURL('m/getevent/declined');
-                setitemID("3")
-            }
-        };
-        const listitem = items.map((item:any)=>(
-            <Popup data={item} key={item.id}/>
-        ));
-          
+    const listitem = items.map((item: any) => (
+        <Popup data={item} key={item.id} />
+    ));
+
 
     return (
         <Auth type={"MANAGER"} >
             <div className={styles.manager_bg}>
-            <Navbar />
-            <div className={styles.manager_index}>
-                <Sidebar id="1" data={{change: selectcomponents}}/>
-                <div className={styles.manager_index_scroll_set}>
-                    
-                    {buyersidebar[0].status?
-                        <div>
-                            <h1>Insigths</h1>
-                            <div className={styles.manager_setting_fix}>
-                                <Homepage />
-                            </div>
-                        </div>
-                    :null}
+                <Navbar />
+                <div className={styles.manager_index}>
+                    <Sidebar id="1" data={{ change: selectcomponents }} />
+                    <div className={styles.manager_index_scroll_set}>
 
-                    {buyersidebar[1].status?
-                        <div>
-                            <ManagerTopBar id2={itemID} data={{change: changeEventList}} />
-                            <div className={styles.manager_index_scroll_set}>
-                                <h1>Events</h1>
-                                <div className={styles.manager_index_container}>
-                                    {listitem}
+                        {buyersidebar[0].status ?
+                            <div>
+                                <h1>Insigths</h1>
+                                <div className={styles.manager_setting_fix}>
+                                    <Homepage />
                                 </div>
                             </div>
-                        </div>
-                    :null}
+                            : null}
 
-                    {buyersidebar[2].status?
-                        <div>
-                            <div onClick={changeSellerList}><SellersTopBar id3={selleritemID} data={{change: changeSellerList}}/></div>
-                            <div className={styles.manager_sellers_main_container}>
-                                <h1>Sellers</h1>
+                        {buyersidebar[1].status ?
+                            <div>
+                                <ManagerTopBar id2={itemID} data={{ change: changeEventList }} />
+                                <div className={styles.manager_index_scroll_set}>
+                                    <h1>Events</h1>
+                                    <div className={styles.manager_index_container}>
+                                        {listitem}
+                                    </div>
+                                </div>
+                            </div>
+                            : null}
+
+                        {buyersidebar[2].status ?
+                            <div>
+                                <div onClick={changeSellerList}><SellersTopBar id3={selleritemID} data={{ change: changeSellerList }} /></div>
                                 <div className={styles.manager_sellers_main_container}>
-                                    {loader?null:<Seller data={selleritems} />}
-                                </div>
-                            </div>
-                        </div>
-                    :null}
-
-                    {buyersidebar[3].status?
-                        <div className={styles.manager_index_scroll_set}>
-                            <h1>Settings</h1>
-                            <div className={styles.manager_settings_container}>
-                                <Profile/>
-                                <div className={styles.manager_settings_container_listinput_container}>
-                                    <div className={styles.manager_settings_container_listinput_container_left}>
-                                        <Arealist />
-                                    </div>
-                                    <div className={styles.manager_settings_container_listinput_container_right}>
-                                        <Categorylist />
+                                    <h1>Sellers</h1>
+                                    <div className={styles.manager_sellers_main_container}>
+                                        {loader ? null : <Seller data={selleritems} />}
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    :null}
+                            : null}
 
+                        {buyersidebar[3].status ?
+                            <div className={styles.manager_index_scroll_set}>
+                                <h1>Settings</h1>
+                                <div className={styles.manager_settings_container}>
+                                    <Profile />
+                                    <div className={styles.manager_settings_container_listinput_container}>
+                                        <div className={styles.manager_settings_container_listinput_container_left}>
+                                            <Arealist />
+                                        </div>
+                                        <div className={styles.manager_settings_container_listinput_container_right}>
+                                            <Categorylist />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            : null}
+
+                    </div>
                 </div>
-            </div>
             </div>
         </Auth>
     );
--- server/routes/manager.js
+++ server/routes/manager.js
@@ -194,7 +194,7 @@ const masking = (data)=>{
           if(data.length>0){
               res.status(200).json(data)
           }else{
-              res.status(200).json("No data Found")
+              res.status(400).json("No data Found")
           }
       })
       .catch(err => res.status(500).json("Server error"))
